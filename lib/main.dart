784a95c435d855a3332497c6c83d945172ef1998
AlfredoSolis2000 <alfredo.solissanchez@utxicotepec.edu.mx>
2023-09-15 14:01:36 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:practica6_200291/src/button.dart';
+
+void main (){
+  
+  runApp(
+    MaterialApp(
+      home: MyButton(),
+      debugShowCheckedModeBanner: false
+
+    )
+  );
+}
